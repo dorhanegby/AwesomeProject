58f4d8cada771a3b8e9e3ec3404d27efc1b0ce0d
dorhanegby <dorhanegbyy@gmail.com>
2017-07-09 14:57:06 +0300

    finished project

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,71 +1,122 @@
 import React, { Component } from 'react';
-import { View, TouchableHighlight , Image, StyleSheet, FlatList, Text} from 'react-native';
+import { View, TouchableHighlight , Image, StyleSheet, FlatList, Text, ActivityIndicator} from 'react-native';
 import {List, ListItem} from 'react-native-elements';
 
-import CategoryHeader from './src/Components/CategoryHeader';
-import FixedHeader from './src/Components/FixedHeader';
 import PlaceHolders from './src/Components/PlaceHolders';
+import Search from 'react-native-search-box';
 
 import api from './utilities/api';
 
 export default class HomeScreen extends Component {
+
+        // Navigation title
+        static navigationOptions = {
+            title: 'Home page',
+            headerTintColor: "blue",
+            headerStyle: {
+                backgroundColor:"green"
+            }
+        };
     constructor(props) {
         super(props);
         this.state = {
             categories: [],
             games: [],
             num_games: 0,
+            page: 0,
+            updating: false,
         }
     }
 
     componentWillMount() {
-        api.getGames().then((res) => {
-            this.setState({
-                categories: res.categories,
-                games: res.games,
-                num_games: res.num_games,
-            })
-        })
+         this.makeRemoteRequest();
     }
 
+    /**
+     * fetching data by category 
+     * 
+     * @param {*String} cat - category to data on
+     */
     dataByCat(cat) {
-    var data = [];
-        for(i=0;i<this.state.num_games;i++) {
-            if(this.state.games[i].categories.includes(cat))
-                data.push(this.state.games[i])
+        var data = [];
+            for(i=0;i<this.state.num_games;i++) {
+                if(this.state.games[i].categories.includes(cat))
+                    data.push(this.state.games[i])
+            }
+
+        return data;
+    }
+    /**
+     *  Searching function
+     */
+    onSearch = (text) => {
+        
+        this.props.navigation.navigate('SearchScreen', {
+            query: text,
+            });
+            
+     }
+
+     /**
+      * requesting more data from api
+      */
+  makeRemoteRequest = () => {
+      api.getGames().then((res) => {
+            this.setState({
+                all_categories: res.categories,
+                categories: this.state.page < res.categories.length ? [...this.state.categories,res.categories[this.state.page]] : this.state.categories,
+                games: res.games,
+                num_games: res.num_games,
+                updating: false
+            })
+        })
+  }
+  /**
+   * Function to handle onEndReached event
+   */
+  handleEnd = () => {
+      if(!this.state.updating) {
+            this.setState({
+                    page: this.state.page + 1,
+                    updating: true
+            },() => {
+                this.makeRemoteRequest();
+            });
         }
-
-    return data;
-}
-
-    
+    }
+    /**
+     * Function that renders an Activity Indicator on the list footer
+     */
+    renderFooter = () => {
+        return (
+            <View>
+                <ActivityIndicator animating  />
+            </View>
+        );
+    }
+  
   render() {
     const { navigation } = this.props;
-    const cat1 = this.state.categories[0]
-    const cat1_data = this.dataByCat(cat1);
-    const cat2 = this.state.categories[1]
-    const cat2_data = this.dataByCat(cat2);
-    const cat3 = this.state.categories[2]
-    const cat3_data = this.dataByCat(cat3);
+    const { page } = this.state.page;
+    console.disableYellowBox = true;
     return (
-      <View style={{flex:1}}>
-         <FixedHeader />
-         <List> 
-            <FlatList
-                data={this.state.games}
-                renderItem={({item}) => 
-                <PlaceHolders navigator ={navigation} category={"cat"} data={item} />
-                }
-            />
-           
-        </List>
+      <View>
+          <Search
+          ref="search_box"
+          onSearch={this.onSearch}
+        />
+            <List> 
+                <FlatList
+                    data={this.state.categories}
+                    renderItem={({item}) =>
+                    <PlaceHolders navigator ={navigation} category={item} data={this.dataByCat(item)} />
+                    }
+                    onEndReached={this.handleEnd}
+                    onEndReachedThreshold={0}
+                    ListFooterComponent={this.renderFooter}
+                   />
+            </List>
       </View>
     );
   }
 }
-
-/*  <PlaceHolders navigator ={navigation} category={cat1} data={cat1_data} />
-            <PlaceHolders navigator ={navigation} category={cat2} data={cat2_data} />
-            <PlaceHolders navigator ={navigation} category={cat3} data={cat3_data} />*/
-
-
